570b4bb54439d6ccf766e291eeebe853c22fb31b
shimarrudz <vic.shima.vss@gmail.com>
2023-07-07 08:43:34 -0300

    feat: applying zod

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,6 @@
 import { knex as setupKnex, Knex } from 'knex'
 
-import 'dotenv/config'
-import { error } from 'console'
+import { env } from './env/index'
 
 if (!process.env.DATABASE_URL) {
     throw new Error('DATABASE_URL env not found')
@@ -9,7 +8,7 @@ if (!process.env.DATABASE_URL) {
 export const config: Knex.Config = ({
     client: 'sqlite',
     connection: {
-        filename: process.env.DATABASE_URL
+        filename: env.DATABASE_URL
     },
     useNullAsDefault: true,
     migrations: {
---
+++ src/env/index.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+
+
+const envSchema = z.object({
+
+    NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+    DATABASE_URL: z.string(),
+    PORT: z.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+    console.error(' Invalid enviorement variables', _env.error.format)
+
+    throw new Error("Invalid enviorement variables")
+}
+
+export const env = _env.data
