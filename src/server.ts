3ee6892e227427b77b19acbb2dca3f12b1e984d0
shimarrudz <vic.shima.vss@gmail.com>
2023-07-07 08:19:37 -0300

    feat: where method

--- src/server.ts
+++ src/server.ts
@@ -6,16 +6,15 @@ import { knex } from "./database";
 const app = fastify();
 
 app.get('/hello', async () => {
-    const transaction = await knex('transactions').insert({
-        id:crypto.randomUUID(),
-        title: "Transação de teste",
-        amount: 1000,
-    })
-    
+    const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
     return transaction
 })
 
-app.listen({
+app
+  .listen({
     port: 3333,
 }).then(() => {
     console.log('HTTP Server Running')
