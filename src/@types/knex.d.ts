7b98aa0aa42d49ad5aeb3772297b09422b31d633
shimarrudz <vic.shima.vss@gmail.com>
2023-07-08 14:08:31 -0300

    aplying: two get requests

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -1,4 +1,14 @@
 // Esta extensão de arquivo, diz que haverá apenas typescript 
 import { Knex } from 'knex'
 
-declare module 'knex/type/tables'
+declare module 'knex/type/tables' {
+    export interface Tables{
+        transactions: {
+            id: string
+            title: string
+            amount: number
+            created_at: string
+            session_id?: string
+        }
+    }
+}
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,11 +1,28 @@
 import { FastifyInstance } from "fastify"
 import { z } from 'zod'
 import { randomUUID } from "node:crypto"
-import crypto from 'node:crypto'
 
 import { knex } from "../database"
 
 export async function transactionsRoutes(app: FastifyInstance) {
+    app.get('/transactions', async () => {
+        const transactions = await knex('transactions').select()
+
+        return { transactions }
+    })
+
+    app.get('/transactions/:id', async (request) => {
+        const getTransactionParamsSchema = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { id } = getTransactionParamsSchema.parse(request.params)
+
+        const transaction = await knex('transactions').where('id', id).first()
+
+        return { transaction }
+    })
+
     app.post('/transactions', async (request, reply) => {
         
         const createTransactionBodySchema = z.object({
