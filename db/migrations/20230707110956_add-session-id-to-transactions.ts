72eba6a52e0eb2e0adbaa1f5469fec1316df9274
shimarrudz <vic.shima.vss@gmail.com>
2023-07-08 14:55:58 -0300

    fix: fixing tables

--- db/migrations/20230707110956_add-session-id-to-transactions.ts
+++ db/migrations/20230707110956_add-session-id-to-transactions.ts
@@ -3,13 +3,13 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.alterTable('transactions', (table) => {
-        table.uuid('session_id').after('id').index()
+        table.uuid('sessionId').after('id').index()
     })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
     await knex.schema.alterTable('transactions', (table) => {
-        table.dropColumn('session_id')
+        table.dropColumn('sessionId')
     })
 }
